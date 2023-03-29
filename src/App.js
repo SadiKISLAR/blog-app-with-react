83cdd552afd79fce6ed9ed6a71b35697ed72243a
SadiKISLAR <sadi22kislar@gmail.com>
2023-03-29 17:33:56 +0300

    app last changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import { useAuthContext } from "./contexts/AuthProvider";
+import AppRouter from "./router/AppRouter";
+import { ToastContainer } from "react-toastify";
+import { ThemeProvider } from "@mui/material/styles";
+import { blueGrey, indigo } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 
 function App() {
+  const { isDark } = useAuthContext();
+
+  const theme = createTheme({
+    palette: {
+      mode: isDark ? "dark" : "light",
+      primary: {
+        main: blueGrey[500],
+      },
+      secondary: {
+        main: indigo[400],
+      },
+    },
+  });
+
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <AppRouter />
+        <ToastContainer />
+      </ThemeProvider>
     </div>
   );
 }
